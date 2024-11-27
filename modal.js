a04747bd406fbad0bb83a57d21ef563c40c81384
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2024-11-27 15:14:34 +0100

    Added a link to modal.js script file.

--- modal.js
+++ modal.js
@@ -1,10 +1,10 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
 
 // DOM Elements
@@ -17,7 +17,7 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+    modalbg.style.display = "block";
+
+    console.log("function launchModal()");
 }
-
-
